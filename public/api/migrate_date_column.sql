f2d6519c8f46f78b9424151fb9b6f81236f078c6
Your Name <your.email@example.com>
2025-10-12 01:48:50 +0800

    Fix MHO recommended foods adding - set meal category and enable recommended button handling

--- public/api/migrate_date_column.sql
+++
@@ -1,5 +0,0 @@
--- Migration script to allow NULL values in date column for MHO recommended foods
--- This allows MHO recommended foods to be stored with date = NULL
-
-ALTER TABLE user_food_history 
-MODIFY COLUMN date DATE NULL;
