7e9b3042ef01a31ff489a3c26335faad0c7a9b22
Your Name <your.email@example.com>
2025-10-13 09:45:06 +0800

    Fix MHO Food Manager to use system template user
    - Change from NULL user_email to 'system@templates.local'
    - Create SQL script to add system template user to community_users
    - Create PHP setup script to execute the SQL
    - System user will store all MHO food templates
    - Templates available to all users based on classification
    - Avoids foreign key constraint issues
    - User-added foods still use real user_email and is_mho_recommended = 0

---
+++ public/api/create_system_template_user.sql
@@ -0,0 +1,35 @@
+-- Create system template user for MHO food templates
+-- This user will store all template foods that can be used by any user
+
+INSERT INTO community_users (
+    email, 
+    name, 
+    municipality, 
+    barangay, 
+    sex, 
+    birthday, 
+    is_pregnant, 
+    weight, 
+    height, 
+    screening_date, 
+    fcm_token, 
+    is_flagged, 
+    status
+) VALUES (
+    'system@templates.local',
+    'System Templates',
+    'SYSTEM',
+    'TEMPLATES',
+    'N/A',
+    '1970-01-01',
+    'N/A',
+    0,
+    0,
+    NOW(),
+    'system',
+    0,
+    1
+) ON DUPLICATE KEY UPDATE 
+    name = 'System Templates',
+    municipality = 'SYSTEM',
+    barangay = 'TEMPLATES';
